6118757069c5868996fc8ce9aa61991898d92804
LUJ95 <lasdi1421@gmail.com>
2025-05-08 14:45:33 +0900

    initial

--- app.js
+++ app.js
@@ -28,32 +28,37 @@ app.get("/api/getFirstCategorys", mongoController.getFirstCategorys);
 app.get("/api/getSecondCategorys", mongoController.getSecondCategorys);
 app.get("/api/getThirdCategorys", mongoController.getThirdCategorys);
 
-
-// Route to handle requests for famous works based on category
 app.get('/api/getFamousWorks', async (req, res) => {
   console.log("1", req.query.name);
-<<<<<<< HEAD
-
-=======
->>>>>>> 242a85446f79b0a5b1ba9ee23f842b582b875ea7
   const category = req.query.name;
   console.log("1", category);
   if (!category) {
     return res.status(400).json({ error: 'Category is required' });
   }
 
-<<<<<<< HEAD
-  const works = await mongoController.getFamousWorks(category);
-  res.json({ works });
-});
-
-
-=======
   //mongo.js 함수로 카테고리 값 갖고 연결
   const works = await mongoController.getFamousWorks(category);
   res.json({ works });
 });
->>>>>>> 242a85446f79b0a5b1ba9ee23f842b582b875ea7
+
+app.get('/api/saveSelectedBook', async (req, res) => {
+  console.log("1", req.query.name);
+  const category = req.query.name;
+  console.log("1", category);
+  if (!category) {
+    return res.status(400).json({ error: 'Category is required' });
+  }
+
+  //mongo.js 함수로 카테고리 값 갖고 연결
+  const works = await mongoController.getFamousWorks(category);
+  res.json({ works });
+});
+
+app.post("/api/saveUserInfo", async (req, res) => {
+  const works = await mongoController.joinSuccess(req.body);
+  res.json({ works });
+});
+
 
 // 서버 시작
 app.listen(PORT, () => {
