3dca579aad7a7be78fba3e72e1596343b160f1c7
LUJ95 <lasdi1421@gmail.com>
2025-05-16 19:22:28 +0900

    2025-05-16(2)

--- services/aiService.js
+++ services/aiService.js
@@ -164,3 +164,9 @@ function saveSelectedBook(book) {
   return books;
 }
 exports.saveSelectedBook = saveSelectedBook;
+
+
+require('dotenv').config();
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
